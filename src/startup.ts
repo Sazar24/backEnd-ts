f9845a09338d543f2e6dff6e99c30ea18fcdf6bb
Sazar24 <budrewicz.cezary@gmail.com>
2018-04-20 16:30:27 +0200

    add base express app and get-route

--- src/startup.ts
+++ src/startup.ts
@@ -1,5 +1,16 @@
 import * as dotenv from 'dotenv';
+
 dotenv.config(); // Loads variables from '.env' file to process.env
+import * as express from 'express';
+const app = express();
 
 
-console.log('dupa!!!!!!!! ');
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world\n');
+});
+
+app.listen(3000,
+    () => console.log("server started at port :3000 ")
+);
